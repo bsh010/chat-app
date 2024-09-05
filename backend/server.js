41341e73cb5f909831ef882fca32e36a10bcb666
Bharat Hardi <bharathardi109@gmail.com>
2024-09-06 02:48:09 +0530

    made changes

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,23 @@ import messageRoutes from '../backend/routes/message.routes.js';
 import userRoutes from '../backend/routes/user.routes.js';
 import connectDB from './db/db.js';
 import {app,server} from './socket/socket.js'; 
+import path from 'path';
 
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 app.use('/api/users',userRoutes);
-
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res)=>{
+   res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 server.listen(PORT,()=>{
    connectDB();
    console.log(`server running on port ${PORT}`);
