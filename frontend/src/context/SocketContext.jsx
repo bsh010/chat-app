1e720c0c02c167ea419e24abec5d1e44598c0fb7
Bharat Hardi <bharathardi109@gmail.com>
2024-09-06 02:59:55 +0530

    add url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -32,7 +32,7 @@ export const SocketContextProvider = ({ children }) => {
             setSocket(null); 
          }
       }
-   }, [authUser])
+   }, [authUser,socket])
    
    return (
       <SocketContext.Provider value={{socket,onlineUsers}}>
