91c1c7d8dd158c4332128c3e3384bba243053bc2
Bharat Hardi <bharathardi109@gmail.com>
2024-11-03 11:50:32 +0530

    logo added

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-00e3.onrender.com", {
+			const socket = io("https://chat-app-00e3.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
